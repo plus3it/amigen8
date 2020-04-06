c370815a62f4c2ab4fcf4d9cba41ef32675490dd
Thomas H Jones II <github@xanthia.com>
2020-04-06 15:21:35 -0400

    Add disk-carving logic

--- DiskSetup.sh
+++ DiskSetup.sh
@@ -49,3 +49,142 @@ function UsageMsg {
    exit 1
 }
 
+# Partition as LVM
+function CarveLVM {
+   local ITER
+   local MOUNTPT
+   local PARTITIONARRAY
+   local PARTITIONSTR
+   local VOLFLAG
+   local VOLNAME
+   local VOLSIZE
+
+   # Whether to use flag-passed partition-string or default values
+   if [ -z ${GEOMETRYSTRING+x} ]
+   then
+       # This is fugly but might(??) be easier for others to follow/update
+       PARTITIONSTR="/:rootVol:4"
+       PARTITIONSTR+=",swap:swapVol:2"
+       PARTITIONSTR+=",/home:homeVol:1"
+       PARTITIONSTR+=",/var:varVol:2"
+       PARTITIONSTR+=",/var/log:logVol:2"
+       PARTITIONSTR+=",/var/log/audit:auditVol:100%FREE"
+   else
+       PARTITIONSTR="${GEOMETRYSTRING}"
+   fi
+
+   # Convert ${PARTITIONSTR} to iterable array
+   IFS=',' read -r -a PARTITIONARRAY <<< "${PARTITIONSTR}"
+
+   # Clear the MBR and partition table
+   dd if=/dev/zero of="${CHROOTDEV}" bs=512 count=1000 > /dev/null 2>&1
+
+   # Lay down the base partitions
+   parted -s "${CHROOTDEV}" -- mktable gpt \
+     mkpart primary "${FSTYPE}" 2048s "${BOOTDEVSZ}" \
+     mkpart primary "${FSTYPE}" "${BOOTDEVSZ}" 100% \
+     set 1 boot on \
+     set 2 lvm
+
+   # Gather info to diagnose seeming /boot race condition
+   if [[ $(grep -q "${BOOTLABEL}" /proc/mounts)$? -eq 0 ]]
+   then
+     tail -n 100 /var/log/messages
+     sleep 3
+   fi
+
+   # Stop/umount boot device, in case parted/udev/systemd managed to remount it
+   # again.
+   systemctl stop boot.mount || true
+
+   # Create /boot filesystem
+   mkfs -t "${FSTYPE}" "${MKFSFORCEOPT}" -L "${BOOTLABEL}" \
+     "${CHROOTDEV}${PARTPRE}1" || \
+       err_exit "Failure creating filesystem - /boot"
+
+   ## Create LVM objects
+
+   # Let's only attempt this if we're a secondary EBS
+   if [[ ${CHROOTDEV} == /dev/xvda ]] || [[ ${CHROOTDEV} == /dev/nvme0n1 ]]
+   then
+      echo "Skipping explicit pvcreate opertion... "
+   else
+      pvcreate "${CHROOTDEV}${PARTPRE}2" || LogBrk 5 "PV creation failed. Aborting!"
+   fi
+
+   # Create root VolumeGroup
+   vgcreate -y "${VGNAME}" "${CHROOTDEV}${PARTPRE}2" || LogBrk 5 "VG creation failed. Aborting!"
+   # Create LVM2 volume-objects by iterating ${PARTITIONARRAY}
+   ITER=0
+   while [[ ${ITER} -lt ${#PARTITIONARRAY[*]} ]]
+   do
+      MOUNTPT="$( cut -d ':' -f 1 <<< "${PARTITIONARRAY[${ITER}]}")"
+      VOLNAME="$( cut -d ':' -f 2 <<< "${PARTITIONARRAY[${ITER}]}")"
+      VOLSIZE="$( cut -d ':' -f 3 <<< "${PARTITIONARRAY[${ITER}]}")"
+
+      # Create LVs
+      if [[ ${VOLSIZE} =~ FREE ]]
+      then
+         # Make sure 'FREE' is given as last list-element
+         if [[ $(( ITER += 1 )) -eq ${#PARTITIONARRAY[*]} ]]
+         then
+            VOLFLAG="-l"
+            VOLSIZE="100%FREE"
+         else
+            echo "Using 'FREE' before final list-element. Aborting..."
+            kill -s TERM " ${TOP_PID}"
+         fi
+      else
+         VOLFLAG="-L"
+         VOLSIZE+="g"
+      fi
+      lvcreate --yes -W y "${VOLFLAG}" "${VOLSIZE}" -n "${VOLNAME}" "${VGNAME}" || \
+        err_exit "Failure creating LVM2 volume '${VOLNAME}'"
+
+      # Create FSes on LVs
+      if [[ ${MOUNTPT} == swap ]]
+      then
+         mkswap "/dev/${VGNAME}/${VOLNAME}"
+      else
+         mkfs -t "${FSTYPE}" "${MKFSFORCEOPT}" "/dev/${VGNAME}/${VOLNAME}" \
+            || err_exit "Failure creating filesystem for '${MOUNTPT}'"
+      fi
+
+      (( ITER+=1 ))
+   done
+
+   # shellcheck disable=SC2053
+   if [[ ${FSTYPE} == ext3 ]] || [[ ${FSTYPE} == ext4 ]]
+   then
+      if [[ $( e2label "${CHROOTDEV}${PARTPRE}1" ) != ${BOOTLABEL} ]]
+      then
+         e2label "${CHROOTDEV}${PARTPRE}1" "${BOOTLABEL}" || \
+            err_exit "Failed to apply desired label to ${CHROOTDEV}${PARTPRE}1"
+      fi
+   elif [[ ${FSTYPE} == xfs ]]
+   then
+      if [[ $( xfs_admin -l "${CHROOTDEV}${PARTPRE}1"  | sed -e 's/"$//' -e 's/^.*"//' ) != ${BOOTLABEL} ]]
+      then
+         xfs_admin -L "${CHROOTDEV}${PARTPRE}1" "${BOOTLABEL}" || \
+            err_exit "Failed to apply desired label to ${CHROOTDEV}${PARTPRE}1"
+      fi
+   else
+      err_exit "Unrecognized fstype [${FSTYPE}] specified. Aborting... "
+   fi
+
+}
+
+# Partition with no LVM
+function CarveBare {
+   # Clear the MBR and partition table
+   dd if=/dev/zero of="${CHROOTDEV}" bs=512 count=1000 > /dev/null 2>&1
+
+   # Lay down the base partitions
+   parted -s "${CHROOTDEV}" -- mklabel msdos mkpart primary "${FSTYPE}" 2048s "${BOOTDEVSZ}" \
+      mkpart primary "${FSTYPE}" ${BOOTDEVSZ} 100%
+
+   # Create FS on partitions
+   mkfs -t "${FSTYPE}" "${MKFSFORCEOPT}" -L "${BOOTLABEL}" "${CHROOTDEV}${PARTPRE}1"
+   mkfs -t "${FSTYPE}" "${MKFSFORCEOPT}" -L "${ROOTLABEL}" "${CHROOTDEV}${PARTPRE}2"
+}
+
