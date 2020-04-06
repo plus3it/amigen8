be8a88a5063a199bbb2e877ca01902cf4cbebb16
Thomas H Jones II <github@xanthia.com>
2020-04-06 16:34:56 -0400

    Initial content

---
+++ PostBuild.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -eu -o pipefail
+#
+# Install primary OS packages into chroot-env
+#
+#######################################################################
+PROGNAME=$(basename "$0")
+CHROOTMNT="${CHROOT:-/mnt/ec2-root}"
+DEBUG="${DEBUG:-UNDEF}"
+
+# Make interactive-execution more-verbose unless explicitly told not to
+if [[ $( tty -s ) -eq 0 ]] && [[ ${DEBUG} == "UNDEF" ]]
+then
+   DEBUG="true"
+fi
+
+
+# Error handler function
+function err_exit {
+   local ERRSTR
+   local ISNUM
+   local SCRIPTEXIT
+
+   ERRSTR="${1}"
+   ISNUM='^[0-9]+$'
+   SCRIPTEXIT="${2:-1}"
+
+   if [[ ${DEBUG} == true ]]
+   then
+      # Our output channels
+      logger -i -t "${PROGNAME}" -p kern.crit -s -- "${ERRSTR}"
+   else
+      logger -i -t "${PROGNAME}" -p kern.crit -- "${ERRSTR}"
+   fi
+
+   # Only exit if requested exit is numerical
+   if [[ ${SCRIPTEXIT} =~ ${ISNUM} ]]
+   then
+      exit "${SCRIPTEXIT}"
+   fi
+}
+
+# Clean yum/DNF history
+function CleanHistory {
+   err_exit "Executing yum clean..." NONE
+   chroot "${CHROOTMNT}" yum clean --enablerepo=* -y packages || \
+     err_exit "Failed executing yum clean"
+
+   err_exit "Nuking DNF history DBs..." NONE
+   chroot "${CHROOTMNT}" rm -rf /var/lib/dnf/history.* || \
+     err_exit "Failed to nuke DNF history DBs"
+
+}
